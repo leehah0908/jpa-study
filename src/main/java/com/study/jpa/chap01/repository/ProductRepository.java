5cf90b40e86612b889c26197c55c179da8ef3e55
leehah <leehah0908@naver.com>
2024-10-17 14:41:04 +0900

    Spring Data JPA를 이용한 CRUD 테스트

--- src/main/java/com/study/jpa/chap01/repository/ProductRepository.java
+++ src/main/java/com/study/jpa/chap01/repository/ProductRepository.java
@@ -3,10 +3,6 @@ package com.study.jpa.chap01.repository;
 import com.study.jpa.chap01.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 // JpaRepository<엔티티 클래스 타입, PK의 타입>
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    void deleteByName(String name);
-    Optional<Product> findByName(String name);
 }
