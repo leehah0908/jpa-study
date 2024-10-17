99471566cabe81f385f6f4d95d674d1b7a090119
leehah <leehah0908@naver.com>
2024-10-17 16:18:55 +0900

    Spring Data JPA Query 아노테이션 사용

--- src/main/java/com/study/jpa/chap02/repository/StudentRepository.java
+++ src/main/java/com/study/jpa/chap02/repository/StudentRepository.java
@@ -2,8 +2,12 @@ package com.study.jpa.chap02.repository;
 
 import com.study.jpa.chap02.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
 
@@ -27,5 +31,34 @@ public interface StudentRepository extends JpaRepository<Student, String> {
 //    List<Student> findByAgeGreaterThan(int age);
 
 //    List<Student> findByAgeLessThan(int age);
+
+    /*
+        > native-sql
+        SELECT 컬럼명 FROM 테이블명
+        WHERE 컬럼 = ?
+
+        > JPQL
+        SELECT 별칭 FROM 엔터티클래스명 AS 별칭
+        WHERE 별칭.필드명 = ?
+    */
+
+    // native SQL 방식
+    @Query(value = "select * from tbl_student where stu_name = :nm or city = :city", nativeQuery = true)
+    List<Student> getStudentByNameOrCity(@Param("nm") String name, @Param("city") String city);
+
+    // JPQL 방식
+    @Query(value = "select st from Student st where st.name = ?1 or st.city = ?2")
+    List<Student> getStudentByNameOrCity2(String name, String city);
+
+    @Query("select st from Student st where st.name like %?1%")
+    List<Student> searchByNameWithJPQL(String name);
+
+    // 단일 조회
+    @Query("select st from Student st where st.city = ?1")
+    Optional<Student> getByCityWithJPQL(String city);
+
+    @Modifying // select 아니면 무조건 붙여야 함
+    @Query("delete from Student st where st.name = ?1 and st.city = ?2")
+    void deleteByNameAndCityWithJPQL(String name, String city);
 }
 
--- src/test/java/com/study/jpa/chap02/repository/StudentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap02/repository/StudentRepositoryTest.java
@@ -2,23 +2,24 @@ package com.study.jpa.chap02.repository;
 
 import com.study.jpa.chap02.entity.Student;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class StudentRepositoryTest {
 
     @Autowired
     StudentRepository studentRepository;
 
-    @BeforeEach
+    @Test
     void insertData() {
         Student s1 = Student.builder()
                 .name("홍길동")
@@ -107,5 +108,44 @@ class StudentRepositoryTest {
         System.out.println(byMajorEndingWith);
     }
 
+    @Test
+    @DisplayName("도시 또는 이름으로 학생 조회")
+    void nativeSQLTest() {
+        // given
+        String name = "홍길동";
+        String city = "제주도";
 
+        // when
+        List<Student> studentByNameOrCity = studentRepository.getStudentByNameOrCity(name, city);
+
+        // then
+        System.out.println(studentByNameOrCity);
+    }
+
+    @Test
+    @DisplayName("JPQL로 like 쿼리 테스트")
+    void JPQLLikeTest() {
+        // given
+        String str = "영";
+
+        // when
+        List<Student> students = studentRepository.searchByNameWithJPQL(str);
+
+        // then
+        System.out.println(students);
+    }
+
+    @Test
+    @DisplayName("JPQL로 삭제하기")
+    void deleteTest () {
+        // given
+        String name = "홍길동";
+        String city = "경기도";
+
+        // when
+        studentRepository.deleteByNameAndCityWithJPQL(name, city);
+
+        // then
+        Assertions.assertEquals(0, studentRepository.findByName(name).size());
+    }
 }
