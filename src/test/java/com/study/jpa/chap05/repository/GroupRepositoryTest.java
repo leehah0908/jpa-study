2220444e0c791bab788bd71901547033fc4c8f41
leehah <leehah0908@naver.com>
2024-10-18 15:45:55 +0900

    QueryDSL 기본 기능 테스트

---
+++ src/test/java/com/study/jpa/chap05/repository/GroupRepositoryTest.java
@@ -0,0 +1,90 @@
+package com.study.jpa.chap05.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.jpa.chap05.entity.Group;
+import com.study.jpa.chap05.entity.Idol;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.study.jpa.chap05.entity.QIdol.idol;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class GroupRepositoryTest {
+
+    @Autowired
+    GroupRepository groupRepository;
+
+    @Autowired
+    IdolRepository idolRepository;
+
+    @Autowired
+    JPAQueryFactory factory;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    void setUp() {
+        //given
+        Group leSserafim = new Group("르세라핌");
+        Group ive = new Group("아이브");
+        groupRepository.save(leSserafim);
+        groupRepository.save(ive);
+
+        Idol idol1 = new Idol("김채원", 24, leSserafim);
+        Idol idol2 = new Idol("사쿠라", 26, leSserafim);
+        Idol idol3 = new Idol("가을", 22, ive);
+        Idol idol4 = new Idol("리즈", 20, ive);
+        idolRepository.save(idol1);
+        idolRepository.save(idol2);
+        idolRepository.save(idol3);
+        idolRepository.save(idol4);
+    }
+
+    @Test
+    @DisplayName("JPQL로 특정이름의 아이돌 조회하기")
+    void jpqlTest() {
+        //given
+        String jpqlQuery = "SELECT i FROM Idol i WHERE i.idolName = ?1";
+
+        //when
+        Idol foundIdol = entityManager.createQuery(jpqlQuery, Idol.class)
+                .setParameter(1, "가을")
+                .getSingleResult();
+
+        //then
+        assertEquals("아이브", foundIdol.getGroup().getGroupName());
+        System.out.println("\n\n\n\n");
+        System.out.println("foundIdol = " + foundIdol);
+        System.out.println("foundIdol.getGroup() = " + foundIdol.getGroup());
+        System.out.println("\n\n\n\n");
+    }
+
+    @Test
+    @DisplayName("queryDsl 특정 이름의 아이돌 조회")
+    void queryDslTest() {
+        // given
+
+        // when
+        Idol findOne = factory
+                .select(idol)
+                .from(idol)
+                .where(idol.idolName.eq("리즈").and(idol.group.groupName.eq("아이브")))
+                .fetchOne();
+
+        // then
+        System.out.println("\n\n\n\n");
+        System.out.println("foundIdol = " + findOne);
+        System.out.println("foundIdol.getGroup() = " + findOne.getGroup().getGroupName());
+        System.out.println("\n\n\n\n");
+    }
+
+}
